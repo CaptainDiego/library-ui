b81a6ab33a8ae7ca97dbfaa2f43b9f6412032165
CaptainDiego <diego.gilon@gmail.com>
2020-05-15 16:26:06 -0500

    Change logo and other public assets

--- app/routes/lead/profile.js
+++ app/routes/lead/profile.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class LeadProfileRoute extends Route {
+	model({ id }) {
+		return {
+			first: 'Quentin',
+			last: 'Gilon',
+			email: 'quentin@manypixels.co',
+			id: `lead-${id}`
+		}
+	}
 }
