69693126fc2ff5950b39974d1d08bc9a461b55f0
CaptainDiego <diego.gilon@gmail.com>
2020-05-15 16:58:52 -0500

    Delete Lead button and action

---
+++ app/controllers/lead/profile.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class LeadProfileController extends Controller {
+	@action
+	deleteLead(lead) {
+		lead.destroyRecord().then(() => {
+			this.transitionToRoute('lead');
+		});
+	}	
+};
--- app/routes/lead/profile.js
+++ app/routes/lead/profile.js
@@ -2,11 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class LeadProfileRoute extends Route {
 	model({ id }) {
-		return {
-			first: 'Quentin',
-			last: 'Gilon',
-			email: 'quentin@manypixels.co',
-			id: `lead-${id}`
-		}
+		return this.store.findRecord('lead', id);
 	}
 }
