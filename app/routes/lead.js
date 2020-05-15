cf86ba1fbb10d1d2a98512bad75c48e664976c44
CaptainDiego <diego.gilon@gmail.com>
2020-05-15 08:32:09 -0500

    Ember Data set up

--- app/routes/lead.js
+++ app/routes/lead.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class LeadRoute extends Route {
 	model() {
-		return fetch('http://localhost:3000/leads')
-			.then(response => response.json());
+		return this.store.findAll('lead');
 	}
 }
+ 
