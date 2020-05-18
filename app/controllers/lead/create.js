fdac2382de98f363677fce0e596b9a28bbf6ced6
CaptainDiego <diego.gilon@gmail.com>
2020-05-18 12:47:29 -0500

    Save lead and redirect to index

--- app/controllers/lead/create.js
+++ app/controllers/lead/create.js
@@ -3,4 +3,13 @@ import { action } from '@ember/object';
 
 
 export default class LeadCreateController extends Controller {
+	@action
+	saveLead(event) {
+		event.preventDefault();
+		let lead = this.store.createRecord('lead', this.model);
+		lead.save()
+			.then(() => {
+				this.transitionToRoute('lead');
+			});
+	}
 }
