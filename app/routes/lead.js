7f20a820d61f7f7cd91b8330950826772275c1f3
Diego Gilon <diego.gilon@gmail.com>
2020-05-18 22:28:36 -0500

    Apply filter enterd in search bar

--- app/routes/lead.js
+++ app/routes/lead.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default class LeadRoute extends Route {
-	model() {
+  queryParams = {
+    search: {
+      refreshModel: true
+    }
+  }
+
+	model(search) {
+    if (search) {
+      return this.store.query('lead', {
+        filter: {
+          query: search.search
+        }
+      });
+    }
+
 		return this.store.findAll('lead');
 	}
 }
- 
